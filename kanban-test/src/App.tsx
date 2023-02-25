2e7e2dfd5e4a20087da827e999d801ff20a70d6f
JuanPinilla198 <juanfepinilla198@gmail.com>
2023-02-25 00:00:01 -0500

    -m "Setup gh-pages"

--- kanban-test/src/App.tsx
+++ kanban-test/src/App.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { KanbanComponent } from '@syncfusion/ej2-react-kanban';
-import { ColumnDirective, ColumnsDirective } from '@syncfusion/ej2-react-kanban/src';
+import { ColumnDirective, ColumnsDirective, KanbanComponent } from '@syncfusion/ej2-react-kanban/src';
 
 class App extends React.Component {
   render() {
       return(<KanbanComponent>
         <ColumnsDirective>
-        <ColumnDirective> headerText='To Do' keyField="Open" </ColumnDirective>
-        <ColumnDirective> headerText='In Progress' keyField="In Progress" </ColumnDirective>
-        <ColumnDirective> headerText='Testing' keyField="Testing" </ColumnDirective>
-        <ColumnDirective> headerText='Close' keyField="Close" </ColumnDirective>
+          <ColumnDirective headerText='To Do' keyField="Open"></ColumnDirective>
+          <ColumnDirective headerText='In Progress' keyField="In Progress"></ColumnDirective>
+          <ColumnDirective headerText='Testing' keyField="Testing" ></ColumnDirective>
+          <ColumnDirective headerText='Close' keyField="Close" ></ColumnDirective>
         </ColumnsDirective>
       </KanbanComponent>);
   }
